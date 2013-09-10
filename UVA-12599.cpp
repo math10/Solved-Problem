1e91536fa64699284e6d3ea2dcf351252c2f3eeb
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2013-09-10 15:18:07 +0600

    Create UVA-12599.cpp

---
+++ UVA-12599.cpp
@@ -0,0 +1,135 @@
+/*
+Author :: MD. Musfiqur Rahman Sanim
+Aust cse 28th Batch
+ID:11.02.04.097
+*/
+
+//{ Template
+using namespace std;
+//{ C-headers
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ C++-headers
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-10
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2 * acos (0.0)
+//}
+typedef long long int64;
+typedef unsigned long long int64u;
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define INF 1<<29
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+#define mx 100005
+//{
+//Intput_Output
+#define II ({ int a; scanf("%d",&a); a;})
+#define IL ({ int64 a; scanf("%lld",&a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define ICA(n) ({ char a[n]; scanf("%s",&a);  a;})
+#define OC printf("Case %d:",cs);
+#define FI freopen("in.txt","r",stdin);
+#define FO freopen("out.txt","w",stdout);
+//}
+//}
+template <class T, class U> inline T max (T &a, U &b) { return a > b ? a : b; }
+template <class T, class U> inline T min (T &a, U &b) { return a < b ? a : b; }
+template <class T, class U> inline T swap (T &a, U &b) { a ^= b ; b = a ^ b ; a ^= b; }
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+vector<int >b,w;
+int main(){
+    int t;
+
+    cin >> t;
+    while( t-- ){
+        b.clear();
+        w.clear();
+        string st;
+        cin >> st;
+
+        int l = st.length();
+
+        if( st[0]=='W' || st[l-1]=='B') {
+            printf("IMPOSSIBLE\n");
+            continue ;
+        }
+
+        rep(i,l){
+            if( st[i]=='B' )
+                b.push_back(i+1);
+            else
+                w.push_back(i+1);
+        }
+        if( l-w[0]+1 == w.size() ){
+            printf("1\n%d",l);
+
+            rep(i,l)
+            printf(" %d",i+1);
+            NL;
+        }
+        else{
+            printf("2\n");
+            int cnt = w[0]+w.size() - 2,take;
+            cout << cnt;
+            rep(i,b.size()){
+                if( b[i] < w[0] )
+                    printf(" %d",b[i]);
+                else{
+                    take=i;
+                    break;
+                }
+            }
+
+            rep(i,w.size()-1)
+            printf(" %d",w[i]);
+            NL;
+            cout << l - cnt;
+
+            for(int i = take ; i < b.size(); i++)
+                printf(" %d",b[i]);
+            printf(" %d\n",w[w.size()-1]);
+        }
+    }
+    return 0;
+}
