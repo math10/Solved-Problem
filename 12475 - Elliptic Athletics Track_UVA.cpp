0111840d742cb861cdec1dc9aa7630b706011993
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2013-09-13 11:51:30 +0600

    geometry + integration

--- 12475 - Elliptic Athletics Track_UVA.cpp
+++ 12475 - Elliptic Athletics Track_UVA.cpp
@@ -83,10 +83,12 @@ double simpson (double a, double b) {
     double ret = 0;
     double N   = int (1e4); // N must be even
     double h   = (b - a) / N;
+    
     for (int i = 0; i <= N; ++i) {
         double x = a + h * i;
         ret += F (x) * ((i == 0 || i == N) ? 1 : (i & 1) ? 4 : 2);
     }
+    
     ret *= (h/3.0);
     return ret;
 }
