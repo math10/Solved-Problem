c60f6030e1c8ee606c77eec8f3a2bfc18bc800d5
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2013-09-20 18:33:18 +0600

    Bitmask

---
+++ 10911 - Forming Quiz Teams.cpp
@@ -0,0 +1,130 @@
+/*
+Author :: MD. Musfiqur Rahman Sanim
+Aust cse 28th Batch
+ID:11.02.04.097
+*/
+
+//{ Template
+using namespace std;
+//{ C-headers
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ C++-headers
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-10
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2 * acos (0.0)
+//}
+typedef long long int64;
+typedef unsigned long long int64u;
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define INF 1<<29
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+#define mx 100005
+//{
+//Intput_Output
+#define II ({ int a; scanf("%d",&a); a;})
+#define IL ({ int64 a; scanf("%lld",&a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define ICA(n) ({ char a[n]; scanf("%s",&a);  a;})
+#define OC printf("Case %d:",cs);
+#define FI freopen("in.txt","r",stdin);
+#define FO freopen("out.txt","w",stdout);
+//}
+//}
+template <class T, class U> inline T max (T &a, U &b) {
+    return a > b ? a : b;
+}
+template <class T, class U> inline T min (T &a, U &b) {
+    return a < b ? a : b;
+}
+template <class T, class U> inline T swap (T &a, U &b) {
+    a ^= b ;
+    b = a ^ b ;
+    a ^= b;
+}
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+int n,x,y;
+string st;
+vector<pii>V;
+db dp[1<<17];
+bool vis[1<<17];
+
+db dis(int i,int j){
+    return sqrt((db)(V[i].first-V[j].first)*(db)(V[i].first-V[j].first) + (db)(V[i].second-V[j].second)*(db)(V[i].second-V[j].second));
+}
+
+db call(int mask){
+     int k = __builtin_popcount(mask);
+     if(k==2*n) return 0.0;
+     if(vis[mask]) return dp[mask];
+     vis[mask] = true;
+     db ret = INF;
+     rep(i,2*n){
+         if(mask & (1 << i)) {continue;}
+         int tmp = mask ^ (1 << i);
+         rep(j,2*n){
+            if(tmp & (1 << j)) {continue;}
+            ret = min(ret,call(tmp ^ (1 << j))+dis(i,j));
+         }
+         break;
+     }
+     return dp[mask] = ret;
+}
+
+int main(){
+    int cs = 1;
+    while(true){
+        V.clear();
+        n = II;
+        if(!n) return 0;
+        rep(i,2*n){
+            cin >> st >> x >> y;
+            V.pb(pii(x,y));
+        }
+        OC;
+        memo(vis,false);
+        printf(" %.2lf\n",call(0));
+        cs++;
+    }
+}
