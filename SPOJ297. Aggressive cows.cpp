7d26251877e200fb08d224b96ce5c02fbbeeb7be
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2013-10-11 19:37:07 +0600

    binary search

---
+++ SPOJ297. Aggressive cows.cpp
@@ -0,0 +1,108 @@
+/*
+Author :: MD. Musfiqur Rahman Sanim
+Aust cse 28th Batch
+ID:11.02.04.097
+*/
+
+//{ Template
+using namespace std;
+//{ C-headers
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ C++-headers
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-10
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2 * acos (0.0)
+//}
+typedef long long int64;
+typedef unsigned long long int64u;
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define INF 1<<29
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+#define mx 100005
+//{
+//Intput_Output
+#define II ({ int a; scanf("%d",&a); a;})
+#define IL ({ int64 a; scanf("%lld",&a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define ICA(n) ({ char a[n]; scanf("%s",&a);  a;})
+#define OC printf("Case %d:",cs);
+#define FI freopen("in.txt","r",stdin);
+#define FO freopen("out.txt","w",stdout);
+//}
+//}
+template <class T, class U> inline T max (T &a, U &b) {
+    return a > b ? a : b;
+}
+template <class T, class U> inline T min (T &a, U &b) {
+    return a < b ? a : b;
+}
+template <class T, class U> inline T swap (T &a, U &b) {
+    a ^= b ;
+    b = a ^ b ;
+    a ^= b;
+}
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+int arr[mx];
+int main(){
+    //FI;
+    int t = II;
+    For(cs,t){
+        int n = II,c = II;
+        int low = 0,high = 0,mid;
+        rep(i,n) arr[i] = II,high = max(high,arr[i]);
+        sort(arr,arr+n);
+        int ans = 0;
+        while(low<=high){
+            mid = (low+high)>>1;
+            int prev = arr[0],cnt = 1;
+            For(i,n-1){
+                if(arr[i] - prev >= mid) prev = arr[i],cnt++;
+            }
+            if(cnt>=c) low = mid+1,ans = mid;
+            else high = mid-1;
+        }
+        cout << ans << endl;
+    }
+}
