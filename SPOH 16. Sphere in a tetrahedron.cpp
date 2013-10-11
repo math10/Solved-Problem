7a071f1d9c37345f9f7046c0b58664d92711e109
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2013-10-11 18:32:49 +0600

    geometry

--- SPOH 16. Sphere in a tetrahedron.cpp
+++ SPOH 16. Sphere in a tetrahedron.cpp
@@ -87,40 +87,40 @@ template <class T, class U> inline T swap (T &a, U &b) {
 //(x/a)^2 + (y/b)^2 = 1;
 
 
-int main(){
+int main() {
     int t = II;
-    For(cs,t){
+    For(cs,t) {
         db u,v,w,U,V,W;
         db Volume = 0,area = 0;
         cin >> u >> v >> w >> W >> V >> U;
 
         //Volume of a tetrahedron start
         Volume = 4*u*u*v*v*w*w;
-		Volume -= u*u*pow(v*v+w*w-U*U,2);
-		Volume -= v*v*pow(w*w+u*u-V*V,2);
-		Volume -= w*w*pow(u*u+v*v-W*W,2);
-		Volume += (v*v+w*w-U*U)*(w*w+u*u-V*V)*(u*u+v*v-W*W);
-		Volume = sqrt(Volume);
-		Volume /= 12;
-		//Volume of tetrahedron end
+        Volume -= u*u*pow(v*v+w*w-U*U,2);
+        Volume -= v*v*pow(w*w+u*u-V*V,2);
+        Volume -= w*w*pow(u*u+v*v-W*W,2);
+        Volume += (v*v+w*w-U*U)*(w*w+u*u-V*V)*(u*u+v*v-W*W);
+        Volume = sqrt(Volume);
+        Volume /= 12;
+        //Volume of tetrahedron end
 
-		//area of a tetrahedron start
-		double s1 = (double) ((U + V + W) / 2);
-		double s2 = (double) ((u + v + W) / 2);
-		double s3 = (double) ((u + V + w) / 2);
-		double s4 = (double) ((U + v + w) / 2);
+        //area of a tetrahedron start
+        double s1 = (double) ((U + V + W) / 2);
+        double s2 = (double) ((u + v + W) / 2);
+        double s3 = (double) ((u + V + w) / 2);
+        double s4 = (double) ((U + v + w) / 2);
 
-		double L1 = sqrt(s1*(s1-U)*(s1-V)*(s1-W));
-		double L2 = sqrt(s2*(s2-u)*(s2-v)*(s2-W));
-		double L3 = sqrt(s3*(s3-u)*(s3-V)*(s3-w));
-		double L4 = sqrt(s4*(s4-U)*(s4-v)*(s4-w));
+        double L1 = sqrt(s1*(s1-U)*(s1-V)*(s1-W));
+        double L2 = sqrt(s2*(s2-u)*(s2-v)*(s2-W));
+        double L3 = sqrt(s3*(s3-u)*(s3-V)*(s3-w));
+        double L4 = sqrt(s4*(s4-U)*(s4-v)*(s4-w));
 
-		area = L1 + L2 + L3 + L4;
-		//area of a tetrahedron end
+        area = L1 + L2 + L3 + L4;
+        //area of a tetrahedron end
 
-		//the radius of a sphere inscribed in the given tetrahedron.
-		double R = (Volume*3.0)/(area);
+        //the radius of a sphere inscribed in the given tetrahedron.
+        double R = (Volume*3.0)/(area);
 
-		printf ("%.4lf\n",R);
+        printf ("%.4lf\n",R);
     }
 }
