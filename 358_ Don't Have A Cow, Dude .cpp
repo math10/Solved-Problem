f73755700a5b3579bcc219bce9a924c8c725924a
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2013-10-17 15:04:49 +0600

    Binary Search + geo

---
+++ 358_ Don't Have A Cow, Dude .cpp
@@ -0,0 +1,100 @@
+//{ Template
+using namespace std;
+//{ C-headers
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ C++-headers
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-10
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2 * acos (0.0)
+//}
+#define max(a,b) (a)>(b)?(a):(b)
+#define min(a,b) (a)<(b)?(a):(b)
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define INF 1<<29
+#define ll long long
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+#define mx 100005
+//{
+//Intput_Output
+#define II ({ int a; scanf("%d",&a); a;})
+#define IL ({ ll a; scanf("%lld",&a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define ICA(n) ({ char a[n]; scanf("%s",&a);  a;})
+#define OC printf("Case %d:",cs);
+#define FI freopen("in.txt","r",stdin);
+#define FO freopen("out.txt","w",stdout);
+//}
+//}
+
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+
+double solve(double mid,double r){
+    double t1 = 2 * acos((mid*mid) / (2 * mid * r) );
+    double t2 = 2 * acos(((r*r)+(r*r) - (mid*mid)) / (2* r * r) );
+    double area = (.5*(t1 - sin(t1))*mid*mid) + (.5*(t2 - sin(t2))*r*r);
+    return area;
+}
+
+int main(){
+    int t = II;
+    while(t--){
+        double r = ID,p = ID;
+        double area = (PI * r * r ) * p;
+        //cout << area << endl;
+        double low = 0,high = r*2;
+        while(fabs(low-high)>EPS){
+            double mid = (low + high) / 2.0;
+            double tmp =solve(mid,r);
+            //cout << mid << ' ' << tmp << endl;
+            if(fabs(tmp-area)<=EPS) break;
+            if(area<tmp) high = mid;
+            else low = mid;
+        }
+        printf("R = %.0lf, P = %.2lf, Rope = %.2lf\n",r,p,(low+high)/2.0);
+        if(t) NL;
+    }
+    return 0;
+}
