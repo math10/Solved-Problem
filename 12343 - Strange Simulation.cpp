ca272924101ac53b6585badc596d504598fcba25
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2013-12-05 20:44:57 +0600

    counting

---
+++ 12343 - Strange Simulation.cpp
@@ -0,0 +1,169 @@
+/*
+Author :: MD. Musfiqur Rahman Sanim
+Aust cse 28th Batch
+ID:11.02.04.097
+*/
+
+
+//{ Template
+using namespace std;
+//{ C-headers
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ C++-headers
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-7
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2*acos (0.0)
+//}
+typedef long long int64;
+typedef unsigned long long int64u;
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+//{
+//Intput_Output
+#define II ({ int a; scanf("%d",&a); a;})
+#define IL ({ int64 a; scanf("%lld",&a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define ICA(n) ({ char a[n]; scanf("%s",&a);  a;})
+#define OC printf("Case %d:",cs);
+//}
+//}
+template <class T, class U> inline T max (T &a, U &b)
+{
+    return a > b ? a : b;
+}
+template <class T, class U> inline T min (T &a, U &b)
+{
+    return a < b ? a : b;
+}
+template <class T, class U> inline T swap (T &a, U &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+const int64 INF = (500000LL * 1000000000LL) + 7LL;
+const int mx = 20000 + 7;
+const db pi = PI;
+int EQ(double d){
+    if ( fabs(d) < EPS ) return 0;
+    return d > EPS ? 1 : -1 ;
+}
+int64 ans[70],one = 1ll;
+int bit_size(int64 num){
+    int cnt= 0;
+    while(num){
+        cnt++;
+        num >>= one;
+    }
+    return cnt;
+}
+
+int main(){
+#ifdef Sanim
+    freopen ("in.txt", "r", stdin);
+    // freopen ("output.txt", "w", stdout);
+#endif
+
+    int t = II;
+    For(cs,t){
+        memo(ans,0);
+        int64 p = IL,q = IL;
+        int bitL = bit_size(p),bitR = bit_size(q);
+        forab(i,bitL,bitR){
+            int64 tmp = one<<(i-1);
+            rep(j,i){
+                if(!j) ans[j] += tmp;
+                else ans[j] += tmp>>one;
+            }
+        }
+
+        int64 num = one<<(bitL-one);
+        num = p - num;
+        int64 tot = one<<(bitL-one);
+        if(num){
+            rep(i,bitL){
+                if(!i) ans[i] -= num;
+                else{
+                    int64 tmp = tot<<one;
+                    int64 P = num/tmp;
+                    ans[i] -= P*tot;
+                    P = num%tmp;
+                    if(P>tot) ans[i] -= (P-tot);
+                }
+                tot >>= one;
+            }
+        }
+        //rep(i,bitR) cout << ans[i] ;
+        //NL;
+        num = one<<bitR;
+        tot = one<<(bitR-1);
+        num--;
+        num = num - q;
+        if(num && q++){
+            rep(i,bitR){
+                if(!i) ans[i] -= num;
+                else{
+                    int64 tmp = tot<<one;
+                    int64 P = num/tmp;
+                    ans[i] -= P*tot;
+                    P = num%tmp;
+                    if(P <= tot) ans[i] -= P;
+                    else ans[i] -= tot;
+                }
+                tot >>= one;
+            }
+        }
+        int64u A = 0;
+        rep(i,bitR) {
+           // cout << ans[i] ;
+            if(ans[i]%2) A += (one<<(bitR-i-one));
+        }
+        //NL;
+        OC;
+        cout <<" " << A << endl;
+    }
+}
