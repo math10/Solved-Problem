9f5d5cb32df070514c321eaedcfeac06a8e6f5cd
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2014-01-07 14:51:15 +0600

    number theory + bs

---
+++ 1644 - Prime Gap.cpp
@@ -0,0 +1,126 @@
+/*
+Author :: MD. Musfiqur Rahman Sanim
+Aust cse 28th Batch
+ID:11.02.04.097
+*/
+
+
+//{ Template
+using namespace std;
+//{ C-headers
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ C++-headers
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-7
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2*acos (0.0)
+//}
+typedef long long int64;
+typedef unsigned long long int64u;
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+//{
+//Intput_Output
+#define II ({ int a; scanf("%d",&a); a;})
+#define IL ({ int64 a; scanf("%lld",&a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define ICA(n) ({ char a[n]; scanf("%s",&a);  a;})
+#define OC printf("Case %d:",cs);
+//}
+//}
+template <class T, class U> inline T max (T &a, U &b)
+{
+    return a > b ? a : b;
+}
+template <class T, class U> inline T min (T &a, U &b)
+{
+    return a < b ? a : b;
+}
+template <class T, class U> inline T swap (T &a, U &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+const int64 INF = (500000LL * 1000000000LL) + 7LL;
+const int mx = 20000 + 7;
+const db pi = PI;
+int EQ(double d){
+    if ( fabs(d) < EPS ) return 0;
+    return d > EPS ? 1 : -1 ;
+}
+
+
+#define PrimeLIMIT 1299719
+#define MAX_TOTAL 100010
+
+unsigned int prime[PrimeLIMIT / 64], pr[MAX_TOTAL], prlen;
+#define gP(n) (prime[n>>6]&(1<<((n>>1)&31)))
+#define rP(n) (prime[n>>6]&=~(1<<((n>>1)&31)))
+void sieve() {
+	unsigned int i,j,sqrtN,i2;
+	memset( prime, -1, sizeof( prime ) );
+	sqrtN = (int64) sqrt ( ( double ) PrimeLIMIT ) + 1;
+	for( i = 3; i < sqrtN; i += 2 ) if( gP( i ) )
+		for( j = i * i, i2 = i << 1; j < PrimeLIMIT; j += i2 ) rP( j );
+	pr[prlen++] = 2;
+	for( i = 3; i < PrimeLIMIT; i += 2 ) if( gP( i ) ) pr[prlen++] = i;
+}
+int main(){
+#ifdef Sanim
+    freopen ("in.txt", "r", stdin);
+    // freopen ("output.txt", "w", stdout);
+#endif
+    sieve();
+    int num;
+    while(cin >> num && num){
+        int low = lower_bound(pr,pr+prlen,num) - pr;
+        if(pr[low] == num) cout << 0 << endl;
+        else{
+            cout << pr[low] - pr[low-1] << endl;
+        }
+    }
+}
