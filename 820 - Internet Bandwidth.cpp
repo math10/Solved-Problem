4a678c59bc945c0974131d58341d243caff9b5d5
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2014-01-23 21:08:42 +0600

    max flow

---
+++ 820 - Internet Bandwidth.cpp
@@ -0,0 +1,188 @@
+/*
+Author :: MD. Musfiqur Rahman Sanim
+Aust cse 28th Batch
+ID:11.02.04.097
+*/
+
+
+//{ Template
+using namespace std;
+//{ C-headers
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ C++-headers
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-7
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2*acos (0.0)
+//}
+typedef long long int64;
+typedef unsigned long long int64u;
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+#define ff first
+#define ss second
+//{
+//Intput_Output
+#define II ({ int a; scanf("%d",&a); a;})
+#define IL ({ int64 a; scanf("%lld",&a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define ICA(n) ({ char a[n]; scanf("%s",&a);  a;})
+#define OC printf("Case %d:",cs);
+//}
+//}
+template <class T, class U> inline T max (T &a, U &b)
+{
+    return a > b ? a : b;
+}
+template <class T, class U> inline T min (T &a, U &b)
+{
+    return a < b ? a : b;
+}
+template <class T, class U> inline T swap (T &a, U &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+const int INF = 1e8 + 7 ;
+const int mx = 20000 + 7;
+const db pi = PI;
+const int mod = 1e9 + 7;
+int EQ(double d) {
+    if ( fabs(d) < EPS ) return 0;
+    return d > EPS ? 1 : -1 ;
+}
+
+const int MX = 10007  ;
+
+const int  MAXE = 100007 ;
+
+int src, snk, nNode, nEdge;
+int Q[MX], fin[MX], pro[MX], dist[MX];
+int flow[MAXE], cap[MAXE], next[MAXE], to[MAXE];
+
+inline void init(int _n) {
+    nNode = _n, nEdge = 0;
+    memo(fin,-1);
+}
+// bidirectional edge **
+inline void add(int u, int v, int c) {
+    to[nEdge] = v, cap[nEdge] = c, flow[nEdge] = 0, next[nEdge] = fin[u], fin[u] = nEdge++;
+    to[nEdge] = u, cap[nEdge] = c, flow[nEdge] = 0, next[nEdge] = fin[v], fin[v] = nEdge++;
+}
+
+bool bfs() {
+    int st, en, i, u, v;
+    memo(dist,-1);
+    dist[src] = st = en = 0;
+    Q[en++] = src;
+    while(st < en) {
+        u = Q[st++];
+        for(i=fin[u]; i>=0; i=next[i]) {
+            v = to[i];
+            if(flow[i] < cap[i] && dist[v]==-1) {
+                dist[v] = dist[u]+1;
+                Q[en++] = v;
+            }
+        }
+    }
+    return dist[snk]!=-1;
+}
+
+int dfs(int u, int fl) {
+    if(u==snk) return fl;
+    for(int &e=pro[u], v, df; e>=0; e=next[e]) {
+        v = to[e];
+        if(flow[e] < cap[e] && dist[v]==dist[u]+1) {
+            df = dfs(v, min(cap[e]-flow[e], fl));
+            if(df>0) {
+                flow[e] += df;
+                flow[e^1] -= df;
+                return df;
+            }
+        }
+    }
+    return 0;
+}
+
+int dinitz() {
+    int ret = 0;
+    int df;
+    while(bfs()) {
+        for(int i=1; i<=nNode; i++) pro[i] = fin[i]; // use temporary source node 0 and main graph 1 to n
+        while(true) {
+            df = dfs(src, INF);
+            if(df) ret += df;
+            else break;
+        }
+    }
+    return ret;
+}
+
+
+int main() {
+
+#ifdef Sanim
+    //freopen ("in.txt", "r", stdin);
+    // freopen ("output.txt", "w", stdout);
+#endif
+    int cs = 1;
+    int n, e ;
+     while( cin >> n && n ) {
+        
+        src = II ;
+        snk = II ;
+        e = II;
+        init(n);
+        rep(i,e) {
+            int u = II , v  = II ,w = II ;
+            if( u != v ) add(u, v, w) ;
+        }
+
+        int flow = dinitz();
+        printf("Network %d\nThe bandwidth is %d.\n\n",cs++,flow);
+    }
+}
