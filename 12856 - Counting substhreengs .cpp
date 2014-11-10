5166e6e8a07d6b9d91b353aeb4122d2c10572e1d
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2014-11-10 22:52:00 +0600

    dp

---
+++ 12856 - Counting substhreengs .cpp
@@ -0,0 +1,113 @@
+/*
+Author :: MD. Musfiqur Rahman Sanim
+Aust cse 28th Batch
+ID:11.02.04.097
+*/
+
+
+//{ Template
+using namespace std;
+//{ headers
+#include<bits/stdc++.h>
+//}
+//{ Loops
+#define forab(i,a,b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i,n) forab (i, 0, (n) - 1)
+#define For(i,n) forab (i, 1, n)
+#define rofba(i,a,b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i,n) rofba (i, 0, (n) - 1)
+#define rof(i,n) rofba (i, 1, n)
+#define forstl(i,s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ Floating-points
+#define EPS 1e-7
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2*acos (0.0)
+//}
+typedef long long int64;
+typedef unsigned long long int64u;
+#define memo(a,v) memset(a,v,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define db double
+#define pb push_back
+#define pii pair<int ,int >
+#define NL puts("")
+#define ff first
+#define ss second
+//{
+//Intput_Output
+#define gc getchar
+template<class T>inline bool read(T &x){int c=gc();int sgn=1;while(~c&&c<'0'|c>'9'){if(c=='-')sgn=-1;c=gc();}for(x=0;~c&&'0'<=c&&c<='9';c=gc())x=x*10+c-'0';x*=sgn;return ~c;}
+#define II ({ int a; read(a); a;})
+#define IL ({ int64 a; read(a);  a;})
+#define ID ({ db a; scanf("%lf",&a);  a;})
+#define IC ({ char a; scanf("%c",&a);  a;})
+#define IS ({ string a; cin >> a;  a;})
+#define OC printf("Case %d:",cs);
+//}
+//}
+#define __(args...) {dbg,args; cerr<<endl;}
+struct debugger{template<typename T> debugger& operator , (const T& v){cerr<<v<<"    "; return *this; }}dbg;
+template <class T, class U> inline T max (T &a, U &b)
+{
+    return a > b ? a : b;
+}
+template <class T, class U> inline T min (T &a, U &b)
+{
+    return a < b ? a : b;
+}
+template <class T, class U> inline T swap (T &a, U &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 Direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[6]={2,1,-1,-2,-1,1};int dy[6]={0,1,1,0,-1,-1}; //Hexagonal Direction
+
+const int64 INF = (1ll)<<50;
+const int mx = 1e6 + 7;
+const int mod = 1000000007 ;
+const db pi = PI;
+int EQ(double d) {
+    if ( fabs(d) < EPS ) return 0;
+    return d > EPS ? 1 : -1 ;
+}
+
+int64 dp[mx][4];
+char ch[mx];
+int main() {
+#ifdef Sanim
+    freopen ("in.txt", "r", stdin);
+    // freopen ("output.txt", "w", stdout);
+#endif
+    while(scanf("%s",ch) == 1){
+        int len = strlen(ch);
+        int64 ans = 0;
+        rep(i,len){
+            if(isdigit(ch[i])){
+                int a = i;
+                while(i<len&&isdigit(ch[i])) i++;
+                int b = i-1;
+                dp[a][0] = 0;
+                dp[a][1] = 0;
+                dp[a][2] = 0;
+                dp[a][(ch[a]-'0')%3] = 1;
+                forab(k,a+1,b){
+                    rep(j,3)dp[k][(j*10+ch[k]-'0')%3] = 0;
+                    rep(j,3){
+                        dp[k][(j*10+ch[k]-'0')%3] += dp[k-1][j];
+                    }
+                    dp[k][(ch[k]-'0')%3] += 1;
+
+                }
+                forab(k,a,b) ans += dp[k][0];
+            }
+        }
+        cout << ans << endl;
+    }
+}
